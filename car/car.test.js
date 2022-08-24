372cc5691351143c8f8839a336c0f5816e2410a2
James Blacklock <jamesblacklock@protonmail.com>
2022-08-24 13:27:03 -0400

    committing to "lecture"

--- car/car.test.js
+++ car/car.test.js
@@ -71,7 +71,7 @@ describe('Car class', () => {
     test('driveAsync', () => {
         return car.driveAsync(20)
             .then(() => {
-                expect(car.odometer).toBe(100);
+                expect(car.odometer).toBe(800);
             });
     });
 })
