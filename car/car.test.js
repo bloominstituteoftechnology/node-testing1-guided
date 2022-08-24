f0b9c1d8ffa374e02eb79aefde80435b6c268d39
James Blacklock <jamesblacklock@protonmail.com>
2022-08-24 12:41:46 -0400

    committing to "lecture"

---
+++ car/car.test.js
@@ -0,0 +1,9 @@
+describe('group1', () => {
+    test('test1', () => {});
+    test('test2', () => {});
+});
+
+describe('group2', () => {
+    test('test1', () => {})
+    test('test2', () => {})
+});
